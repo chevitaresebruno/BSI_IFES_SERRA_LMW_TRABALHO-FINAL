0140557ac3611a2e7eecc4dee97bb4e7544c6891
Bruno <chevitarese.bruno@gmail.com>
2024-03-22 19:58:30 -0300

    terminando o site

--- back-end/blog/urls.py
+++ back-end/blog/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('aula_um/', views.aula_um, name='aula_um/'),
-    path('aula_dois/', views.aula_dois, name='aula_dois/')
+    path('aula_dois/', views.aula_dois, name='aula_dois/'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
