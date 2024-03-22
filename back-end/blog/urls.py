97bd3ea9a3e4b97fde7c122b3989bf25a03da5d6
Bruno <chevitarese.bruno@gmail.com>
2024-03-22 15:22:59 -0300

    salvando trabalho

--- back-end/blog/urls.py
+++ back-end/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('aula_um/', views.aula_um, name='aula_um/'),
+    path('aula_dois/', views.aula_dois, name='aula_dois/')
 ]
--- back-end/blog/views.py
+++ back-end/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+def aula_um(request):
+    return render(request, 'blog/aula_um.html', {})
+
+def aula_dois(request):
+    return render(request, 'blog/aula_dois.html', {})
